595f67df80fc070180389aaee9ce0a7364dbf40d
akzn <40191741+akzn@users.noreply.github.com>
2021-07-07 18:22:44 +0700

    Update lamp-install.sh

--- lamp-install.sh
+++ lamp-install.sh
@@ -4,10 +4,10 @@ echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 echo -e "\n\nInstalling Apache2 Web server\n"
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+sudo apt-get install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y
 
 echo -e "\n\nInstalling PHP & Requirements\n"
-sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php7.0-mcrypt php7.0-mysql -y
+sudo apt-get install libapache2-mod-php php php-common php-curl php-dev php-gd php-pear php-mysql -y
 
 echo -e "\n\nInstalling MySQL\n"
 sudo apt-get install mysql-server mysql-client -y
